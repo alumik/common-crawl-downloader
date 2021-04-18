738a632b075c83c57d7372f0498d3b94b1326380
Zhong Zhenyu <nczzy1997@gmail.com>
2021-04-19 01:55:58 +0800

    Reformat database script

---
+++ database/comcrawl_data.sql
@@ -0,0 +1,47 @@
+create table server
+(
+    id int auto_increment
+        primary key,
+    ip varchar(15) not null
+);
+
+create table data
+(
+    id             int auto_increment
+        primary key,
+    uri            varchar(256) charset utf8 not null,
+    size           int         default 0     not null,
+    date           datetime                  null,
+    process_state  tinyint(11) default 0     not null comment '0 Not processed
+1 Processing
+2 Processed',
+    download_state tinyint(11) default 0     not null comment '0 Waiting for download
+1 Downloading
+2 Downloaded
+3 Failed',
+    server         int                       null,
+    `year_month`   varchar(30) charset utf8  null,
+    constraint data_uri_uindex
+        unique (uri),
+    constraint data_server_id_fk
+        foreign key (server) references server (id)
+);
+
+create table process
+(
+    id     int auto_increment
+        primary key,
+    data   int           not null,
+    size   int default 0 not null,
+    date   datetime      null,
+    server int           null,
+    uri    varchar(256)  null,
+    constraint process_data_uindex
+        unique (data),
+    constraint process_uri_uindex
+        unique (uri),
+    constraint process_data_id_fk
+        foreign key (data) references data (id),
+    constraint process_server_id_fk
+        foreign key (server) references server (id)
+);
