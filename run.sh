cd1b3c9b178e9917a3103ed83d03dafe345e8555
Zhong Zhenyu <nczzy1997@gmail.com>
2021-04-23 10:46:21 +0800

    Fix: CommandNotFoundError: Your shell has not been properly configured to use 'conda activate'

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 mkdir -p log
 for (( i=1; i<=$1; i++ ))
 do
-    screen -dmS comcrawl-$i sh -c "conda activate comcrawl; python src/main.py > log/log-$i.log 2>&1;"
+    screen -dmS comcrawl-$i sh -c "source activate;conda activate comcrawl; python src/main.py >> log/log-$i.log 2>&1;"
 done
