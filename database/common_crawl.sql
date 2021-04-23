84f80033b16934f6836cf00f156833997bf02886
Zhong Zhenyu <nczzy1997@gmail.com>
2021-04-23 12:58:52 +0800

    Update database SQL file

--- database/common_crawl.sql
+++ database/common_crawl.sql
@@ -10,18 +10,18 @@ create table data
     id             int auto_increment
         primary key,
     uri            varchar(256) charset utf8 not null,
-    size           int default 0 not null,
-    started_at     datetime null,
-    finished_at    datetime null,
-    process_state  tinyint(11) default 0 not null comment '0 Not processed
+    size           int         default 0     not null,
+    started_at     datetime                  null,
+    finished_at    datetime                  null,
+    process_state  tinyint(11) default 0     not null comment '0 Not processed
 1 Processing
 2 Processed',
-    download_state tinyint(11) default 0 not null comment '0 Waiting for download
+    download_state tinyint(11) default 0     not null comment '0 Waiting for download
 1 Downloading
 2 Downloaded
 3 Failed',
-    id_worker      int null,
-    archive        varchar(30) charset utf8 null,
+    id_worker      int                       null,
+    archive        varchar(30) charset utf8  null,
     constraint data_uri_uindex
         unique (uri),
     constraint data_worker_id_fk
@@ -34,9 +34,9 @@ create table process
         primary key,
     id_data      int           not null,
     size         int default 0 not null,
-    processed_at datetime null,
-    id_worker    int null,
-    uri          varchar(256) null,
+    processed_at datetime      null,
+    id_worker    int           null,
+    uri          varchar(256)  null,
     constraint process_data_uindex
         unique (id_data),
     constraint process_uri_uindex
@@ -46,4 +46,3 @@ create table process
     constraint process_worker_id_fk
         foreign key (id_worker) references worker (id)
 );
-
