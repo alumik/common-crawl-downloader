cd057866aa1a17a06a60eec4a46ef4f57c7be4cc
Zhong Zhenyu <nczzy1997@gmail.com>
2021-06-14 15:48:22 +0800

    Update utility scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p log
-
 for ((i = 1; i <= $1; i++)); do
-  screen -dmS common-crawl-"$i" bash -c "source activate;conda activate common-crawl; python src/main.py >> log/log-$i.log 2>&1;"
+  screen -dmS common-crawl-"$i" bash -c "source activate;conda activate common-crawl; python src/main.py;"
 done
--- stop_and_update.sh
+++ stop_and_update.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-for scr in $(screen -ls | awk '{print $1}'); do
+for scr in $(screen -ls common-crawl- | awk '{print $1}'); do
   screen -S "$scr" -X stuff '^C'
 done
-
-rm -r log
 
 git fetch --all
 git reset --hard origin/main
 git pull
-
-chmod +x ./*.sh
